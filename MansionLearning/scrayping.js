3d45f866eb87d51d191d7634b08516a3a25f791e
takeyuki197 <takeyuki197@gmail.com>
2018-05-21 22:52:31 +0900

    Fix codes

--- MansionLearning/scrayping.js
+++ MansionLearning/scrayping.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 function sleep(time) {
     return new Promise((resolve, reject) => {
@@ -9,57 +10,151 @@ function sleep(time) {
 }
 
 const RootAddress = "https://www.athome.co.jp";
-const MainAddress = "https://www.athome.co.jp/chintai/tokyo/nerima-st/list";
+const MainAddress = "https://www.athome.co.jp/mansion/chuko/tokyo/list/";
 
-exports.getScrapedData = function(options, callback){
+/**
+ * 
+ * @param {*} outputCSVName 
+ * @param {*} options 
+ * @param {*} callback 
+ */
+exports.getScrapedData = function(outputCSVName, options, callback){
 
-    //puppeteer.launch({headless: false, slowMo: 250}).then(async browser => {
+    //puppeteer.launch({devtools: true, slowMo: 250}).then(async browser => {
     puppeteer.launch().then(async browser => {
         const page = await browser.newPage();
-        await page.goto(MainAddress, { waitUntil:"load" });
+        await page.setJavaScriptEnabled(true);
+        await page.goto(MainAddress, { waitUntil: 'networkidle0' });
 
-        const dataBukkenTitles = await page.evaluate(() => {
-            const node = document.querySelectorAll("h2.p-property__title");
-            const data = [];
-            for(item of node) {
-                const bukkenName = item.innerText;
-                const bukkenWebAddress = item.getElementsByTagName('a')[0].getAttribute('href');
-                const array = [bukkenName, bukkenWebAddress];
-                //data[bukkenName] = bukkenWebAddress;
-                data.push(array);
+        let preBukkenNumStr, postBukkenNumStr;
+
+        preBukkenNumStr = await page.evaluate(() => {return document.querySelector("p.item-list_result.left span.counter span.num").innerText;});
+        await page.select('select[name="MENSEKI"]', 'kt109');
+        await page.select('select[name="EKITOHO"]', 'ke003');
+        await page.waitFor(1000);    
+        await page.waitForFunction('document.querySelector("p.item-list_result.left span.counter span.num").innerText !== "' + preBukkenNumStr + '"');
+        postBukkenNumStr = await page.evaluate(() => {return document.querySelector("p.item-list_result.left span.counter span.num").innerText;});
+        console.log("The number of Bukken becomes " + preBukkenNumStr + ' => ' + postBukkenNumStr);
+        preBukkenNumStr = postBukkenNumStr;
+
+        await page.click('img#dev-replaceSecondImage', { waitUntil: 'networkidle0' });
+        await page.waitFor(1000);
+        const frameData = await page.frames();
+        let iModal = frameData.length - 1;
+        for (let i = 0; i < frameData.length; ++i) {
+            //console.log('frame name ' + String(i) + ': ' + frameData[i].name());
+            if (frameData[i].name().indexOf('152680') === 0) {
+                iModal = i;
+                break;
             }
-            return data;
+        }
+        await frameData[iModal].waitFor(1000);
+        await frameData[iModal].waitForSelector('input#dummy_ov_01');
+        await frameData[iModal].evaluate(() => {
+            var a = document.getElementById('dummy_ov_01');
+            var event = new MouseEvent('click', {});
+            a.dispatchEvent(event);
         });
-        console.log(dataBukkenTitles);
-        
-        const dataBukkenDetails = {};
-        for(let i = 0; i < Math.min(dataBukkenTitles.length, options.maxDataSize); ++i) {
-            const subPage = await browser.newPage();
-            await subPage.goto(RootAddress + dataBukkenTitles[i][1], { waitUntil: "load" });
+        await frameData[iModal].waitForSelector('img[alt="検索結果を見る"]');
+        await frameData[iModal].click('img[alt="検索結果を見る"]');
 
-            const dataDetail = await subPage.evaluate(() => {
-                const node = document.querySelectorAll("dl.data.typeTable");
-                const data = {};
+        await page.waitFor(1000);
+        await page.waitForFunction('document.querySelector("p.item-list_result.left span.counter span.num").innerText !== "' + preBukkenNumStr + '"');
+        postBukkenNumStr = await page.evaluate(() => { return document.querySelector("p.item-list_result.left span.counter span.num").innerText; });
+        console.log("The number of Bukken becomes " + preBukkenNumStr + ' => ' + postBukkenNumStr);
+
+        const bukkenNumStr = postBukkenNumStr;
+        const bukkenNum = parseInt(bukkenNumStr.split(',').join('').trim());
+        const pageNum = Math.floor(bukkenNum / 30) + 1;
+        console.log("The number of page is " + pageNum);
+
+        const maxDataSize = options.maxDataSize || -1;
+        const dataBukkenDetails = {};
+        const csv_data = [];
+        const csv_header = [];
+        let dataNum = 0;
+        for (let iPage = 1; iPage <= pageNum; ++iPage) {
+            if (iPage >= 2) {
+                const pageText = await page.evaluate(() => {
+                    return document.querySelector("p.item-list_result.left").innerText;
+                });
+                const searchString = 'a[page="' + String(iPage) + '"';
+                await page.click(searchString);
+                await page.waitFor(1000);
+                await page.waitForFunction('document.querySelector("p.item-list_result.left").innerText !== "' + pageText + '"');
+                const postPageText = await page.evaluate(() => {return document.querySelector("p.item-list_result.left").innerText;});
+                console.log('Page changes from "' + pageText + '" to "' + postPageText + '"');
+            }   
+
+            const dataBukkenTitles = await page.evaluate(() => {
+                const node = document.querySelectorAll("p.heading.object-title");
+                const data = [];
                 for(item of node) {
-                    const dt = item.getElementsByTagName('dt');
-                    const dd = item.getElementsByTagName('dd');
-                    for(let j = 0; j < dt.length; ++j) {
-                        data[dt[j].innerText] = dd[j].innerText
-                    }
+                    const bukkenName = item.innerText;
+                    const bukkenWebAddress = item.getElementsByTagName('a')[0].getAttribute('href');
+                    const array = [bukkenName, bukkenWebAddress];
+                    //data[bukkenName] = bukkenWebAddress;
+                    data.push(array);
                 }
                 return data;
             });
-            dataBukkenDetails[dataBukkenTitles[i][0]] = dataDetail;
-            console.log(dataDetail);
+            
+            for(let i = 0; i < dataBukkenTitles.length; ++i) {
+                dataNum += 1;
+                if (maxDataSize >= 0 && dataNum > options.maxDataSize) {
+                    break;
+                }
 
-            await sleep(3000);
+                const subPage = await browser.newPage();
+                await subPage.goto(RootAddress + dataBukkenTitles[i][1], { waitUntil: "domcontentloaded" });
 
-            await subPage.close();
+                const dataDetail = await subPage.evaluate(() => {
+                    const node = document.querySelector("section#item-detail_data").querySelectorAll("tr");
+                    const data = {};
+                    for(item of node) {
+                        const th = item.getElementsByTagName('th');
+                        const td = item.getElementsByTagName('td');
+                        for(let j = 0; j < th.length; ++j) {
+                            data[th[j].innerText] = td[j].innerText.split('\n').join(' ')
+                        }
+                    }
+                    return data;
+                });
+                dataBukkenDetails[dataBukkenTitles[i][0]] = dataDetail;
+                console.log("Get bukken data: " + dataBukkenTitles[i][0]);
+
+                if (csv_header.length === 0) {
+                    let line = '"タイトル"';
+                    for (let key of Object.keys(dataDetail)) {
+                        csv_header.push(key);
+                        line += ',"' + key + '"';
+                    }
+                    line += '\n';
+                    fs.appendFileSync(outputCSVName, line);
+                }
+                let line = '';
+                line += '"' + dataBukkenTitles[i][0] + '"';
+                for (let key of csv_header) {
+                    line += ',"' + dataDetail[key] + '"';
+                }
+                line += '\n';
+                fs.appendFileSync(outputCSVName, line);
+
+                await sleep(2000);
+                await subPage.close();
+            }
+
+            if (maxDataSize >= 0 && dataNum > maxDataSize) {
+                break;
+            }
         }
-        console.log(dataBukkenDetails);
-
+        
         await browser.close();
 
-        return callback(dataBukkenDetails);
+        const ret = {
+            json: dataBukkenDetails,
+            csv: csv_data
+        }
+        return callback(ret);
     });
 }
