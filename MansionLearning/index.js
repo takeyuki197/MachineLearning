4c6c2753f8d6e53be8087aceae662ffecfdc82a2
takeyuki197 <takeyuki197@gmail.com>
2018-06-08 08:55:57 +0900

    Add codes

--- MansionLearning/index.js
+++ MansionLearning/index.js
@@ -9,15 +9,26 @@ const options = {
     maxDataSize: -1
 }
 
-const dest = ["練馬", "中野坂上"]
-scrayping.getTransitTime(dest, function(err, data){
-    console.log(data);
+
+const json_data = fs.readFileSync('./time_FromShinjuku.json', 'utf8');
+const transit_data = JSON.parse(json_data);
+const content = fs.readFileSync('./list_eki.csv', 'utf8');
+const dest = content.split('\r\n').map(function (element) { return element.trim()});
+scrayping.getTransitTime(transit_data, dest, function(err, data_array, data_dict){
+    //console.log(data_array);
+    //console.log(data_dict);
+    if (err) {
+        console.log(err.message);
+    }
+    fs.writeFileSync('./time_FromShinjuku.json', JSON.stringify(data_dict, null, "\t"));
 });
+
 
 fs.writeFileSync(outputCSVName, '');
 scrayping.getScrapedData(outputCSVName, options, function(data){
     fs.writeFileSync('./data/bunjou_' + todayString + '.json', JSON.stringify(data.json, null, "\t"));
 });
+
 
 //controlDB.query(function(err, res){
 //    console.log(res);
